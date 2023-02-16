23acf5b2316efa3a762726af317ec77c9d53cd63
Santiago Buitrago <santiago.buitrago@noatec.co>
2023-02-16 15:41:56 -0500

    *

--- code/db_manager.py
+++ code/db_manager.py
@@ -12,20 +12,17 @@ class DEAP_Manager:
     def show_sample(self):
         pass
 
-    def show_subject_data_shape(self, subject):
-        pass
-
     def get_file_for_subject(self, sub):
         with open(f'{self.db_path}/s'+ (('0' + str(sub)) if sub < 10 else str(sub)) + '.dat', 'rb') as file:
             subject = pickle.load(file, encoding='latin1')
             
         self.file[f"subject{sub}"] = subject
 
-    def get_data_for_subject(self, subject):
-        return self.file[f"subject{subject}"]['data']
+    def get_data_for_subject(self, sub):
+        return self.file[f"subject{sub}"]['data']
     
-    def get_labels_for_subject(self, subject):
-        return self.file[f"subject{subject}"]['labels']
+    def get_labels_for_subject(self, sub):
+        return self.file[f"subject{sub}"]['labels']
     
-    def get_subject_shape(self, subject):
-        return self.file[f"subject{subject}"].shape
+    def get_subject_shape(self, sub):
+        return self.file[f"subject{sub}"].shape
