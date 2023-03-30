e8cc382f2743bc6e549491012e5bfb3817cf0098
Santiago Buitrago <sbuitragoo@unal.edu.co>
2023-03-29 21:27:05 -0500

    added windowing and trail splitting

--- code/pre_process.py
+++ code/pre_process.py
@@ -39,6 +39,43 @@ def filter_data(input_data, subjects: List[int], wl: int, wh: int):
     print(f"Filtered data sample: {filtered_signals[f'subject{1}'][0, :, :]}")
     return filtered_signals
 
+def apply_window(input_data, subjects):
+    post_processed_data = {}
+    window_size = 128
+    overlapping = 0.5
+
+    print(f"Starting windowing with window size of {window_size} and overlapping of {overlapping*100}%")
+
+    for subject in subjects:
+        subject_data = input_data[f"subject{subject}"]
+        post_processed_data[f"subject{subject}"] = np.zeros((subject_data.shape[0], subject_data.shape[1], int(subject_data.shape[2] / 2)))
+        for trial in range(subject_data.shape[0]):
+            for i in range(post_processed_data[f"subject{subject}"].shape[2] / window_size):
+                if (i == 0):
+                    post_processed_data[f"subject{subject}"][trial, :, i:i*window_size] = subject_data[trial, :, i:i*window_size]
+                else:
+                    post_processed_data[f"subject{subject}"][trial, :, i:i*window_size] = subject_data[trial, :, i*window_size*overlapping:(i*window_size*overlapping)+window_size]
+    print(f"Finished windowing")
+    return post_processed_data
+
+def split_trial_into_stack(input_data, subjects):
+    splitted_data = {}
+    window_size = 128
+
+    print(f"Starting trail splitting for each subject, begining with a shape per subject of: {input_data[f'subject{1}'].shape}")
+
+    for subject in subjects:
+        subject_data = input_data[f"subject{subject}"]
+        splitted_data[f"subject{subject}"] = np.zeros((subject_data.shape[0]*32, subject_data.shape[1], subject_data.shape[2]))
+        for original_trial in range(subject_data.shape[0]):
+            for new_trial in range(splitted_data[f"subject{subject}"].shape[0]):
+                splitted_data[f"subject{subject}"][new_trial, :, :window_size] = subject_data[original_trial, :, new_trial:new_trial*window_size]
+
+    print(f"Finished trail splitting successfully with a new size per subject of: {splitted_data[f'subject{subject}'].shape}")
+    return splitted_data
+            
+
+
 def label_preprocessing():
     # Read labels
     labels = pd.read_csv('labels.csv')
@@ -63,6 +100,12 @@ def pre_process(db_path: str):
     cut_data = get_data_cut(dm, subjects, 3)
     
     print("Sample shape:", cut_data[f"subject{1}"].shape)
+
+    sampled_data = apply_window(cut_data, subjects)
+
+    splitted_data = split_trial_into_stack(sampled_data, subjects)
+
+    print(f"Final shape: {splitted_data[f'subject{1}'].shape}")
 
     # How the data_preprocessed_python provided by DEAP owners already has been filtered between 4 and 45 Hz, this step is omitted 
     # filtered_data = filter_data(cut_data, subjects, 4, 45)
