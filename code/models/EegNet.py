9ff75ea914bf73444cd189adda759a3cf4366951
Santiago Buitrago <sbuitragoo@unal.edu.co>
2023-04-11 21:23:10 -0500

    added batchnorm and relu between the bottleneck conv layers

--- code/models/EegNet.py
+++ code/models/EegNet.py
@@ -20,25 +20,40 @@ def EEGNet_Full_3D(input_shape=(6,32,128), num_classes=1, WF=0.5):
     inputs = Input(shape=input_shape)
 
     # Temporal convolutional block using 3D convolutions
-    x = Conv3D(int(16*WF), (input_shape[0], 1, 1), padding='same', strides=(1,2,2))(inputs)
+    x = Conv3D(int(16*WF), (1, 1, 1), padding='same', strides=(1,2,2))(inputs)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
     # x = Dropout(0.25)(x)
 
     # Depthwise separable convolutional block using 3D depthwise separable convolutions
     irb1 = Conv3D(int(16*WF), (1, 1, 1), padding='same', activation="relu")(x)
+    irb1 = BatchNormalization()(irb1)
+    irb1 = Activation('relu')(irb1)
     irb1 = DepthwiseConv3D(kernel_size=(1, 1, 1), padding='valid')(irb1)
+    irb1 = BatchNormalization()(irb1)
+    irb1 = Activation('relu')(irb1)
     irb1 = Conv3D(int(16*WF), (1, 1, 1), padding='same', activation="linear")(irb1)
+    irb1 = BatchNormalization()(irb1)
     irb1 = Add()([x, irb1])
 
     irb2 = Conv3D(int(16*WF), (1, 1, 1), padding='same', activation="relu")(irb1)
+    irb2 = BatchNormalization()(irb2)
+    irb2 = Activation('relu')(irb2)
     irb2 = DepthwiseConv3D(kernel_size=(3, 3, 3), padding='valid')(irb2)
+    irb2 = BatchNormalization()(irb2)
+    irb2 = Activation('relu')(irb2)
     irb2 = Conv3D(int(16*WF), (1, 1, 1), padding='same', activation="linear")(irb2)
+    irb2 = BatchNormalization()(irb2)
     irb2 = Add()([irb1, irb2])
 
     irb3 = Conv3D(int(16*WF), (1, 1, 1), padding='same', activation="relu")(irb2)
+    irb3 = BatchNormalization()(irb3)
+    irb3 = Activation('relu')(irb3)
     irb3 = DepthwiseConv3D(kernel_size=(3, 3, 3), padding='valid')(irb3)
+    irb3 = BatchNormalization()(irb3)
+    irb3 = Activation('relu')(irb3)
     irb3 = Conv3D(int(16*WF), (1, 1, 1), padding='same', activation="linear")(irb3)
+    irb3 = BatchNormalization()(irb3)
     irb3 = Add()([irb2, irb3])
 
     x = AveragePooling3D(pool_size=(1,1,1))(irb3)
@@ -46,12 +61,14 @@ def EEGNet_Full_3D(input_shape=(6,32,128), num_classes=1, WF=0.5):
     x = Conv3D(16, (input_shape[0], 1, 1), padding='valid')(x)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
-    x = Dropout(0.25)(x)
+    x = Dropout(0.5)(x)
 
     # Flatten the output and pass it through a fully connected layer for classification
     x = Flatten()(x)
     x = Dense(64, 'relu')(x)
+    x = Dropout(0.5)(x)
     x = Dense(32, 'relu')(x)
+    x = Dropout(0.5)(x)
     x = Dense(8, 'relu')(x)
     x = Dense(num_classes, activation='softmax')(x)
 
